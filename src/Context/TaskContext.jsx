16077b9c6bfb4b7617e4e1747efc8967f560ba8e
Felix Ramon <frfyfelix@gmail.com>
2022-10-12 15:48:55 +0200

    first commit

---
+++ src/Context/TaskContext.jsx
@@ -0,0 +1,35 @@
+import {createContext, useState, useEffect} from 'react'
+import { Tasks as roca } from "../Task";
+
+export const TaskContext = createContext()
+
+export function TaskContextProvider(props) {
+  const [tasks, setTasks] = useState([]);
+
+  function createTask(task) {
+    setTasks([
+      ...tasks,
+      { title: task.title, id: tasks.length, description: task.description, },
+    ]);
+  }
+
+  function deleteTask(taskId) {
+    setTasks(tasks.filter(task => task.id !== taskId))
+  } 
+
+  useEffect(() => {
+    setTasks(roca);
+  }, []);
+   
+  return (
+    <TaskContext.Provider value={{
+      tasks: tasks,
+      deleteTask: deleteTask,
+      createTask: createTask
+    }}>
+        {props.children}
+    </TaskContext.Provider>
+  )
+}
+
+export default TaskContext
