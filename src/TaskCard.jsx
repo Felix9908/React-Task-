bdd5e132475e2499abd6b7cc27f3010b2531b195
Felix9908 <frfyfelix@gmail.com>
2023-12-17 21:27:03 -0500

    se arregla las tarjetas, traduce a ingles y otros detalles

--- src/TaskCard.jsx
+++ src/TaskCard.jsx
@@ -2,61 +2,66 @@ import { useContext, useState } from "react";
 import { TaskContext } from "./Context/TaskContext";
 
 function TaskCard({ task }) {
-  const { deleteTask, updateTask, editDescription, setEditDescription } = useContext(TaskContext);
+  const { deleteTask, updateTask, editDescription, setEditDescription } =
+    useContext(TaskContext);
   const [activeUpdate, setActiveUpdate] = useState(null);
 
   return (
     <div className="bg-gray-800 text-white p-4 rounded-md">
-      <h1 className="text-xl font-bold capitalize">{task.title}</h1>
-      {activeUpdate == task.id ? (
-        <>
-          <input
-            type="text"
-            className="text-black"
-            placeholder="editar comentario"
-            value={editDescription}
-            onChange={(e) => setEditDescription(e.target.value)}
-          />
-        </>
-      ) : (
-        <>
-          <p className="text-gray-300 text-sm">{task.description}</p>
-        </>
-      )}
-      {activeUpdate ? (
-        <>
-          <button
-            className="bg-blue-500 px-2 py-1 rounded-md mt-4 hover:bg-blue-400"
-            onClick={() => {
-              updateTask({ id: task.id, newDescription: editDescription });
-              setActiveUpdate(null);
-            }}
-          >
-            Guardar
-          </button>
-          <button
-            className="bg-red-500 px-2 py-1 rounded-md mt-4 hover:bg-red-400"
-            onClick={() => setActiveUpdate(null)}
-          >
-            Cancelar
-          </button>
-        </>
-      ) : (
-        <>
-          <button
-            className="bg-red-500 px-2 py-1 rounded-md mt-4 hover:bg-red-400"
-            onClick={() => deleteTask(task.id)}
-          >
-            Eliminar Tarea
-          </button>
-          <button
-            className="bg-blue-500 px-2 py-1 rounded-md mt-4 hover:bg-blue-400"
-            onClick={() => setActiveUpdate(task.id)}
-          >
-            Editar Tarea
-          </button>
-        </>
-      )}
+      <div>
+        <h1 className="text-xl font-bold capitalize">{task.title}</h1>
+        {activeUpdate == task.id ? (
+          <>
+            <input
+              type="text"
+              className="text-black"
+              placeholder="Edit your comment"
+              value={editDescription}
+              onChange={(e) => setEditDescription(e.target.value)}
+            />
+          </>
+        ) : (
+          <>
+            <p className="text-gray-300 text-sm">{task.description}</p>
+          </>
+        )}
+      </div>
+      <div>
+        {activeUpdate === task.id ? (
+          <>
+            <button
+              className="bg-blue-500 px-2 py-1 rounded-md mt-4 hover:bg-blue-400"
+              onClick={() => {
+                updateTask({ id: task.id, newDescription: editDescription });
+                setActiveUpdate(null);
+              }}
+            >
+              Save
+            </button>
+            <button
+              className="bg-red-500 px-2 py-1 rounded-md mt-4 hover:bg-red-400"
+              onClick={() => setActiveUpdate(null)}
+            >
+              Cansel
+            </button>
+          </>
+        ) : (
+          <>
+            <button
+              className="bg-red-500 px-2 py-1 rounded-md mt-4 hover:bg-red-400"
+              onClick={() => deleteTask(task.id)}
+            >
+              Delete task
+            </button>
+            <button
+              className="bg-blue-500 px-2 py-1 rounded-md mt-4 hover:bg-blue-400"
+              onClick={() => setActiveUpdate(task.id)}
+            >
+              Edit task
+            </button>
+          </>
+        )}
+      </div>
     </div>
   );
 }
--- src/TaskForm.jsx
+++ src/TaskForm.jsx
@@ -19,20 +19,20 @@ function TaskForm() {
   return (
     <div className="max-w-md mx-auto">
       <form onSubmit={handleSubmit} className="bg-slate-800 p-10 mb-4">
-        <h1 className="text-2xl font-bold text-white mb-3">Crea tu tarea</h1>
+        <h1 className="text-2xl font-bold text-white mb-3">Create your task</h1>
         <input
-          placeholder="Escribe tu tarea"
+          placeholder="Write your title"
           onChange={(e) => setTitle(e.target.value)}
           value={title}
           className="bg-slate-300 p-3 w-full mb-2"
         />
         <textarea
           className="bg-slate-300 p-3 w-full mb-2"
-          placeholder="Escribe la descripcion de la tarea"
+          placeholder="Write your description"
           onChange={(e) => setDescription(e.target.value)}
           value={description}
         ></textarea>
-        <button className="bg-indigo-500 px-3 py-1 text-white">Guardar</button>
+        <button className="bg-indigo-500 px-3 py-1 text-white">Save</button>
       </form>
     </div>
   );
